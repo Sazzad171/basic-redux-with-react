b5077e839d0c57f488d890d334a26d9befd63644
Sazzad171 <sajjad.cse.diu@gmail.com>
2023-07-11 11:07:44 +0600

    update

---
+++ src/services/actions/layoutAction.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+import {
+    GET_LAYOUT_REQUEST,
+    GET_LAYOUT_SUCCESS,
+    GET_LAYOUT_FAILED
+} from "../constants/layoutConstant";
+
+// action for fetch layouts data
+export const getAllLayouts = () => async (dispatch) => {
+    dispatch({
+        type: GET_LAYOUT_REQUEST
+    });
+    try {
+        const res = await axios.get("https://layout.layoutdesign.io/api/version_1/layouts?&per_page=10");
+        dispatch({
+            type: GET_LAYOUT_SUCCESS,
+            payload: res.data
+        });
+    }
+    catch (err) {
+        dispatch({
+            type: GET_LAYOUT_FAILED,
+            payload: err.message
+        })
+    }
+}
