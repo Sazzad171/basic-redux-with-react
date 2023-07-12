e8b5ad8df9a30cae273df153074c75d071fd5474
Sazzad171 <sajjad.cse.diu@gmail.com>
2023-07-12 23:17:51 +0600

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,18 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunk from "redux-thunk";
 
 import counterReducer from "../services/reduces/counterReducer";
 import layoutReducer from "../services/reduces/layoutReducer";
 
+// combine
+const rootreducer = combineReducers({
+    counterReducer: counterReducer,
+    layoutReducer: layoutReducer
+});
+
 // const store = createStore(counterReducer);
-const store = createStore(layoutReducer,
+const store = createStore(
+    rootreducer,
     applyMiddleware(thunk));
 
 export default store;
